8d93592217b15acd8f62c58a3bc9e01238cd3e5a
Raoul Van den Berge <raoulvdberge@gmail.com>
2015-12-29 22:42:40 +0100

    wireless transmitter texture

--- src/main/java/storagecraft/block/BlockWirelessTransmitter.java
+++ src/main/java/storagecraft/block/BlockWirelessTransmitter.java
@@ -1,10 +1,14 @@
 package storagecraft.block;
 
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import storagecraft.StorageCraft;
 import storagecraft.StorageCraftGUI;
@@ -13,9 +17,29 @@ import storagecraft.tile.TileWirelessTransmitter;
 // @TODO: This texture behaves differently
 public class BlockWirelessTransmitter extends BlockMachine
 {
+	public static final PropertyBool WORKING = PropertyBool.create("working");
+
 	public BlockWirelessTransmitter()
 	{
 		super("wireless_transmitter");
+	}
+
+	@Override
+	protected BlockState createBlockState()
+	{
+		return new BlockState(this, new IProperty[]
+			{
+				DIRECTION,
+				CONNECTED,
+				WORKING
+			});
+	}
+
+	@Override
+	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos)
+	{
+		return super.getActualState(state, world, pos)
+			.withProperty(WORKING, ((TileWirelessTransmitter) world.getTileEntity(pos)).isWorking());
 	}
 
 	@Override
