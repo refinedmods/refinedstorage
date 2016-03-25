a1affcb5880f13a1acc2d065ac55e12cd07a9380
Tom Erik <zyberwax@hotmail.com>
2016-03-25 22:58:00 +0100

    Fixes RF draw incorrect, now draws correct amount every tick

--- src/main/java/refinedstorage/tile/TileController.java
+++ src/main/java/refinedstorage/tile/TileController.java
@@ -46,6 +46,14 @@ public class TileController extends TileBase implements IEnergyReceiver, INetwor
         }
 
         if (!worldObj.isRemote) {
+        	switch (getType()) {
+            	case NORMAL:
+            		energy.extractEnergy(energyUsage, false);
+            		break;
+            	case CREATIVE:
+            		energy.setEnergyStored(energy.getMaxEnergyStored());
+            		break;
+        		}
             int lastEnergy = energy.getEnergyStored();
 
             if (ticks % 20 == 0) {
@@ -96,15 +104,6 @@ public class TileController extends TileBase implements IEnergyReceiver, INetwor
 
                 for (TileMachine machine : machines) {
                     energyUsage += machine.getEnergyUsage();
-                }
-
-                switch (getType()) {
-                    case NORMAL:
-                        energy.extractEnergy(energyUsage, false);
-                        break;
-                    case CREATIVE:
-                        energy.setEnergyStored(energy.getMaxEnergyStored());
-                        break;
                 }
 
                 if (lastEnergy != energy.getEnergyStored()) {
