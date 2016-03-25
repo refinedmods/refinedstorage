307205359eb75d4ae8d5a7b7702ae2913ab60fdc
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-03-25 23:23:01 +0100

    Make the energy draw actual RF/t

--- src/main/java/refinedstorage/tile/TileController.java
+++ src/main/java/refinedstorage/tile/TileController.java
@@ -48,6 +48,15 @@ public class TileController extends TileBase implements IEnergyReceiver, INetwor
         if (!worldObj.isRemote) {
             int lastEnergy = energy.getEnergyStored();
 
+            switch (getType()) {
+                case NORMAL:
+                    energy.extractEnergy(energyUsage, false);
+                    break;
+                case CREATIVE:
+                    energy.setEnergyStored(energy.getMaxEnergyStored());
+                    break;
+            }
+
             if (ticks % 20 == 0) {
                 if (!isActive()) {
                     disconnectAll();
@@ -95,15 +104,6 @@ public class TileController extends TileBase implements IEnergyReceiver, INetwor
 
                     for (TileMachine machine : machines) {
                         energyUsage += machine.getEnergyUsage();
-                    }
-
-                    switch (getType()) {
-                        case NORMAL:
-                            energy.extractEnergy(energyUsage, false);
-                            break;
-                        case CREATIVE:
-                            energy.setEnergyStored(energy.getMaxEnergyStored());
-                            break;
                     }
                 }
 
