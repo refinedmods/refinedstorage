0e868cbed31a1b8306b4ce018448f8586bb3256c
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-03-28 23:35:23 +0200

    Fire crafting events and stuff

--- src/main/java/refinedstorage/container/slot/SlotGridCraftingResult.java
+++ src/main/java/refinedstorage/container/slot/SlotGridCraftingResult.java
@@ -5,6 +5,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.inventory.SlotCrafting;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import refinedstorage.container.ContainerGrid;
 import refinedstorage.tile.TileGrid;
 
@@ -23,6 +24,10 @@ public class SlotGridCraftingResult extends SlotCrafting {
 
     @Override
     public void onPickupFromSlot(EntityPlayer player, ItemStack stack) {
+        FMLCommonHandler.instance().firePlayerCraftingEvent(player, stack, craftingMatrix);
+
+        onCrafting(stack);
+
         grid.onCrafted(container);
     }
 }
