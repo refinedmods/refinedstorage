60d5f6b9cfe73cc8e85465ba58c894de578f345f
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-05-05 16:55:12 +0200

    Fix

--- src/main/java/refinedstorage/tile/autocrafting/task/BasicCraftingTask.java
+++ src/main/java/refinedstorage/tile/autocrafting/task/BasicCraftingTask.java
@@ -71,7 +71,7 @@ public class BasicCraftingTask implements ICraftingTask {
             ItemStack input = pattern.getInputs()[i];
 
             if (!satisfied[i] && !childTasks[i]) {
-                builder.append("- ").append(input.getUnlocalizedName()).append("\n");
+                builder.append("- ").append(input.getDisplayName()).append("\n");
 
                 missingItems++;
             }
