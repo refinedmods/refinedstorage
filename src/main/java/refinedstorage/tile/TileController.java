6abb531cb981c229981c316ccb0e1f601d77f895
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-05-19 12:15:15 +0200

    Fixes

--- src/main/java/refinedstorage/tile/TileController.java
+++ src/main/java/refinedstorage/tile/TileController.java
@@ -204,7 +204,6 @@ public class TileController extends TileBase implements IEnergyReceiver, ISynchr
     public void syncMachines() {
         this.wirelessGridRange = 0;
         this.energyUsage = 0;
-        this.itemGroups.clear();
         this.storages.clear();
         this.patterns.clear();
 
