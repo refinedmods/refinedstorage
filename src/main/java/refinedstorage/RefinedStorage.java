f45d6cbd49331627e93518391e6d7febc8391444
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-07-13 15:29:38 +0200

    Version increment to 0.8.10

--- src/main/java/refinedstorage/RefinedStorage.java
+++ src/main/java/refinedstorage/RefinedStorage.java
@@ -22,7 +22,7 @@ import java.util.List;
 @Mod(modid = RefinedStorage.ID, version = RefinedStorage.VERSION)
 public final class RefinedStorage {
     public static final String ID = "refinedstorage";
-    public static final String VERSION = "0.8.9";
+    public static final String VERSION = "0.8.10";
 
     @SidedProxy(clientSide = "refinedstorage.proxy.ClientProxy", serverSide = "refinedstorage.proxy.ServerProxy")
     public static CommonProxy PROXY;
