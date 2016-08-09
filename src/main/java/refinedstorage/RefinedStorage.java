840e5dfc384229427961a961257ee8f46c2e99b5
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-08-09 13:33:36 +0200

    Update dependencies

--- src/main/java/refinedstorage/RefinedStorage.java
+++ src/main/java/refinedstorage/RefinedStorage.java
@@ -19,7 +19,7 @@ import refinedstorage.proxy.CommonProxy;
 public final class RefinedStorage {
     public static final String ID = "refinedstorage";
     public static final String VERSION = "0.8.16";
-    public static final String DEPENDENCIES = "required-after:mcmultipart";
+    public static final String DEPENDENCIES = "required-after:mcmultipart@[1.2.1,);";
 
     @SidedProxy(clientSide = "refinedstorage.proxy.ClientProxy", serverSide = "refinedstorage.proxy.ServerProxy")
     public static CommonProxy PROXY;
