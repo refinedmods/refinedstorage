122ea38e8acd508a2e25f89911f63e6af1d82e9a
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-08-16 23:57:20 +0200

    Start autocrafting task for buckets if no buckets are in the system

--- src/main/java/refinedstorage/apiimpl/storage/fluid/FluidUtils.java
+++ src/main/java/refinedstorage/apiimpl/storage/fluid/FluidUtils.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidContainerItem;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import refinedstorage.api.autocrafting.ICraftingPattern;
 import refinedstorage.api.network.INetworkMaster;
 import refinedstorage.api.network.NetworkUtils;
 
@@ -48,6 +49,12 @@ public final class FluidUtils {
 
                 if (result != null) {
                     result.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null).fill(NetworkUtils.extractFluid(network, fluidStack, Fluid.BUCKET_VOLUME), true);
+                } else {
+                    ICraftingPattern pattern = NetworkUtils.getPattern(network, EMPTY_BUCKET);
+
+                    if (pattern != null) {
+                        network.addCraftingTask(network.createCraftingTask(pattern));
+                    }
                 }
             }
         }
