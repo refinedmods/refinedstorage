5ce4d7205b01997c3c5818928a7179f1520d5101
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-09-17 13:47:31 +0200

    Update docs

--- src/main/java/refinedstorage/api/solderer/SoldererRecipe.java
+++ src/main/java/refinedstorage/api/solderer/SoldererRecipe.java
@@ -3,6 +3,7 @@ package refinedstorage.api.solderer;
 import net.minecraft.item.ItemStack;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * A basic solderer recipe.
@@ -14,9 +15,9 @@ public class SoldererRecipe implements ISoldererRecipe {
     private ItemStack[] rows;
 
     /**
-     * @param result   The result
-     * @param duration The duration in ticks
-     * @param rows     The rows of this recipe, has to be 3 rows (null for an empty row)
+     * @param result   the result
+     * @param duration the duration in ticks
+     * @param rows     the rows of this recipe, has to be 3 rows (null for an empty row)
      */
     public SoldererRecipe(@Nonnull ItemStack result, int duration, ItemStack... rows) {
         if (rows.length != 3) {
@@ -28,16 +29,28 @@ public class SoldererRecipe implements ISoldererRecipe {
         this.rows = rows;
     }
 
+    /**
+     * @param row the row in the solderer that we want the stack for (between 0 - 2)
+     * @return a stack for that row, null if there is no stack
+     */
     @Override
+    @Nullable
     public ItemStack getRow(int row) {
         return rows[row];
     }
 
+    /**
+     * @return the result stack
+     */
     @Override
+    @Nonnull
     public ItemStack getResult() {
         return result;
     }
 
+    /**
+     * @return the time it takes to complete this recipe
+     */
     @Override
     public int getDuration() {
         return duration;
