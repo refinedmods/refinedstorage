5ae70030e9cbbbddcf295f1d227225488a009836
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-10-03 20:29:48 +0200

    Bump to 1.2

--- src/main/java/refinedstorage/RefinedStorage.java
+++ src/main/java/refinedstorage/RefinedStorage.java
@@ -18,7 +18,7 @@ import refinedstorage.proxy.CommonProxy;
 @Mod(modid = RefinedStorage.ID, version = RefinedStorage.VERSION, guiFactory = RefinedStorage.GUI_FACTORY, dependencies = RefinedStorage.DEPENDENCIES)
 public final class RefinedStorage {
     public static final String ID = "refinedstorage";
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.2";
     public static final String DEPENDENCIES = "required-after:Forge@[12.18.1.2088,);required-after:mcmultipart@[1.2.1,);after:JEI@[3.11.0,);";
     public static final String GUI_FACTORY = "refinedstorage.gui.config.ModGuiFactory";
 
