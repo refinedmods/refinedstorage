48eabfbb0938c7a071f0929dd27e46fa2b8d15d0
way2muchnoise <jens.baetens@live.be>
2016-10-05 16:04:42 +0200

    update forge and remove unused imports

--- src/main/java/refinedstorage/integration/jei/RecipeTransferHandlerPattern.java
+++ src/main/java/refinedstorage/integration/jei/RecipeTransferHandlerPattern.java
@@ -13,8 +13,6 @@ import refinedstorage.network.MessageProcessingPatternEncoderTransfer;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 public class RecipeTransferHandlerPattern implements IRecipeTransferHandler<ContainerProcessingPatternEncoder> {
