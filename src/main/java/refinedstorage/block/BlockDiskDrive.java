f81a4e21d22575b2619326c15702d69180afa5c1
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-10-07 21:51:21 +0200

    Fixed some clients not starting up due to too many Disk Drive model permutations, fixes #417

--- src/main/java/refinedstorage/block/BlockDiskDrive.java
+++ src/main/java/refinedstorage/block/BlockDiskDrive.java
@@ -17,17 +17,6 @@ import refinedstorage.RefinedStorageGui;
 import refinedstorage.tile.TileDiskDrive;
 
 public class BlockDiskDrive extends BlockNode {
-    private static final PropertyInteger STORED = PropertyInteger.create("stored", 0, 7);
-
-    private static final PropertyBool FILLED_0 = PropertyBool.create("filled_0");
-    private static final PropertyBool FILLED_1 = PropertyBool.create("filled_1");
-    private static final PropertyBool FILLED_2 = PropertyBool.create("filled_2");
-    private static final PropertyBool FILLED_3 = PropertyBool.create("filled_3");
-    private static final PropertyBool FILLED_4 = PropertyBool.create("filled_4");
-    private static final PropertyBool FILLED_5 = PropertyBool.create("filled_5");
-    private static final PropertyBool FILLED_6 = PropertyBool.create("filled_6");
-    private static final PropertyBool FILLED_7 = PropertyBool.create("filled_7");
-
     public BlockDiskDrive() {
         super("disk_drive");
     }
@@ -35,39 +24,6 @@ public class BlockDiskDrive extends BlockNode {
     @Override
     public TileEntity createTileEntity(World world, IBlockState state) {
         return new TileDiskDrive();
-    }
-
-    @Override
-    public BlockStateContainer createBlockState() {
-        return createBlockStateBuilder()
-            .add(STORED)
-            .add(FILLED_0)
-            .add(FILLED_1)
-            .add(FILLED_2)
-            .add(FILLED_3)
-            .add(FILLED_4)
-            .add(FILLED_5)
-            .add(FILLED_6)
-            .add(FILLED_7)
-            .build();
-    }
-
-    @Override
-    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
-        TileDiskDrive diskDrive = (TileDiskDrive) world.getTileEntity(pos);
-
-        state = super.getActualState(state, world, pos);
-        state = state.withProperty(STORED, Math.max(0, diskDrive.getStoredForDisplay()));
-        state = state.withProperty(FILLED_0, diskDrive.getFilled()[0]);
-        state = state.withProperty(FILLED_1, diskDrive.getFilled()[1]);
-        state = state.withProperty(FILLED_2, diskDrive.getFilled()[2]);
-        state = state.withProperty(FILLED_3, diskDrive.getFilled()[3]);
-        state = state.withProperty(FILLED_4, diskDrive.getFilled()[4]);
-        state = state.withProperty(FILLED_5, diskDrive.getFilled()[5]);
-        state = state.withProperty(FILLED_6, diskDrive.getFilled()[6]);
-        state = state.withProperty(FILLED_7, diskDrive.getFilled()[7]);
-
-        return state;
     }
 
     @Override
--- src/main/java/refinedstorage/tile/TileDiskDrive.java
+++ src/main/java/refinedstorage/tile/TileDiskDrive.java
@@ -86,7 +86,6 @@ public class TileDiskDrive extends TileNode implements IItemStorageProvider, IFl
     private static final String NBT_MODE = "Mode";
     private static final String NBT_STORED = "Stored";
     private static final String NBT_TYPE = "Type";
-    private static final String NBT_FILLED = "Filled_%d";
 
     private ItemHandlerBasic disks = new ItemHandlerBasic(8, this, IItemValidator.STORAGE_DISK) {
         @Override
@@ -132,27 +131,11 @@ public class TileDiskDrive extends TileNode implements IItemStorageProvider, IFl
     private int mode = IFilterable.WHITELIST;
     private int type = IType.ITEMS;
 
-    private int stored = 0;
-    private boolean[] filled = new boolean[8];
-
     public TileDiskDrive() {
         dataManager.addWatchedParameter(PRIORITY);
         dataManager.addWatchedParameter(COMPARE);
         dataManager.addWatchedParameter(MODE);
         dataManager.addWatchedParameter(TYPE);
-    }
-
-    @Override
-    public void update() {
-        if (!worldObj.isRemote) {
-            if (stored != getStoredForDisplay()) {
-                stored = getStoredForDisplay();
-
-                updateBlock();
-            }
-        }
-
-        super.update();
     }
 
     @Override
@@ -264,34 +247,6 @@ public class TileDiskDrive extends TileNode implements IItemStorageProvider, IFl
     }
 
     @Override
-    public NBTTagCompound writeUpdate(NBTTagCompound tag) {
-        super.writeUpdate(tag);
-
-        tag.setInteger(NBT_STORED, stored);
-
-        for (int i = 0; i < 8; ++i) {
-            tag.setBoolean(String.format(NBT_FILLED, i), disks.getStackInSlot(i) != null);
-        }
-
-        return tag;
-    }
-
-    @Override
-    public void readUpdate(NBTTagCompound tag) {
-        stored = tag.getInteger(NBT_STORED);
-
-        for (int i = 0; i < 8; ++i) {
-            filled[i] = tag.getBoolean(String.format(NBT_FILLED, i));
-        }
-
-        super.readUpdate(tag);
-    }
-
-    public boolean[] getFilled() {
-        return filled;
-    }
-
-    @Override
     public int getCompare() {
         return compare;
     }
@@ -313,36 +268,6 @@ public class TileDiskDrive extends TileNode implements IItemStorageProvider, IFl
         this.mode = mode;
 
         markDirty();
-    }
-
-    public int getStoredForDisplay() {
-        if (!worldObj.isRemote) {
-            float stored = 0;
-            float capacity = 0;
-
-            for (int i = 0; i < disks.getSlots(); ++i) {
-                ItemStack disk = disks.getStackInSlot(i);
-
-                if (disk != null) {
-                    int diskCapacity = disk.getItem() == RefinedStorageItems.STORAGE_DISK ? EnumItemStorageType.getById(disk.getItemDamage()).getCapacity() : EnumFluidStorageType.getById(disk.getItemDamage()).getCapacity();
-
-                    if (diskCapacity == -1) {
-                        return 0;
-                    }
-
-                    stored += disk.getItem() == RefinedStorageItems.STORAGE_DISK ? ItemStorageNBT.getStoredFromNBT(disk.getTagCompound()) : FluidStorageNBT.getStoredFromNBT(disk.getTagCompound());
-                    capacity += diskCapacity;
-                }
-            }
-
-            if (capacity == 0) {
-                return 0;
-            }
-
-            return (int) Math.floor((stored / capacity) * 7F);
-        }
-
-        return stored;
     }
 
     @Override
