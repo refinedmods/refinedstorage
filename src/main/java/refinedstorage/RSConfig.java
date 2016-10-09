7803aca5a04e2eecc7ca9febd89716feaa193b37
InusualZ <wesmoret98@gmail.com>
2016-10-09 14:47:53 -0400

    Grammatical fix

--- src/main/java/refinedstorage/RSConfig.java
+++ src/main/java/refinedstorage/RSConfig.java
@@ -131,7 +131,7 @@ public final class RSConfig {
         networkTransmitterPerBlockUsage = config.getFloat("networkTransmitterPerBlock", ENERGY, 4, 0, Float.MAX_VALUE, "The additional energy per block that the Network Transmitter uses, gets rounded up");
         networkReceiverUsage = config.getInt("networkReceiver", ENERGY, 15, 0, Integer.MAX_VALUE, "The energy used by Network Receivers");
         diskManipulatorUsage = config.getInt("diskManipulator", ENERGY, 3, 0, Integer.MAX_VALUE, "The energy used by Disk Manipulators");
-        euConversion = config.getInt("euConversion", ENERGY, 4, 0, Integer.MAX_VALUE, "The amount of RS that equal 1 EU. 4 by default.");
+        euConversion = config.getInt("euConversion", ENERGY, 4, 0, Integer.MAX_VALUE, "The amount of RS that equals 1 EU");
         //endregion
 
         //region Controller
