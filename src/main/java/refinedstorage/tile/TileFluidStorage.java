7ff1a8ec18918905f97217a2b8187104e38ebd37
InusualZ <wesmoret98@gmail.com>
2016-10-09 12:51:11 -0400

    Consistency

--- src/main/java/refinedstorage/tile/TileFluidStorage.java
+++ src/main/java/refinedstorage/tile/TileFluidStorage.java
@@ -53,7 +53,8 @@ public class TileFluidStorage extends TileNode implements IFluidStorageProvider,
             }
 
             FluidStack result  = super.insertFluid(stack, size, simulate);
-            if(voidExcess && result != null) {
+            if (voidExcess && result != null) {
+				// Simulate should not matter as the fluids are voided anyway
                 result.amount = -result.amount;
             }
 
