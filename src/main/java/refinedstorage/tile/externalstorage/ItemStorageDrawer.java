81785330fed28ba36e2a1b6126973eca00db1d80
Geldorn <geldorn.dev@gmail.com>
2016-10-11 18:32:57 +0200

    Coding style correction.

--- src/main/java/refinedstorage/tile/externalstorage/ItemStorageDrawer.java
+++ src/main/java/refinedstorage/tile/externalstorage/ItemStorageDrawer.java
@@ -49,8 +49,7 @@ public class ItemStorageDrawer extends ItemStorageExternal {
             if (!simulate && remainingSpace > 0) {
                 if (drawer.isEmpty()) {
                     drawer.setStoredItemRedir(stack, inserted);
-                }
-                else {
+                } else {
                     drawer.setStoredItemCount(stored + inserted);
                 }
             }
