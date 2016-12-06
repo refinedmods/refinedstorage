b513b4e70476fdf779a160c5926164dfd90bee02
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-12-06 19:35:57 +0100

    Require minimum Forge version

--- src/main/java/com/raoulvdberge/refinedstorage/RS.java
+++ src/main/java/com/raoulvdberge/refinedstorage/RS.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
-@Mod(modid = RS.ID, version = RS.VERSION, guiFactory = RS.GUI_FACTORY)
+@Mod(modid = RS.ID, version = RS.VERSION, dependencies = RS.DEPENDENCIES, guiFactory = RS.GUI_FACTORY)
 public final class RS {
     static {
         FluidRegistry.enableUniversalBucket();
@@ -22,6 +22,7 @@ public final class RS {
 
     public static final String ID = "refinedstorage";
     public static final String VERSION = "1.3.3";
+    public static final String DEPENDENCIES = "required-after:forge@[13.19.1.2188,);";
     public static final String GUI_FACTORY = "com.raoulvdberge.refinedstorage.gui.config.ModGuiFactory";
 
     @SidedProxy(clientSide = "com.raoulvdberge.refinedstorage.proxy.ProxyClient", serverSide = "com.raoulvdberge.refinedstorage.proxy.ProxyCommon")
