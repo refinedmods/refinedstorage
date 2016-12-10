979429d5cec4a90d20ef288dd88d29374f213b6e
Raoul Van den Berge <raoulvdberge@gmail.com>
2016-12-10 13:17:06 +0100

    Bump to 1.2.11

--- src/main/java/com/raoulvdberge/refinedstorage/RS.java
+++ src/main/java/com/raoulvdberge/refinedstorage/RS.java
@@ -22,7 +22,7 @@ public final class RS {
     }
 
     public static final String ID = "refinedstorage";
-    public static final String VERSION = "1.2.10";
+    public static final String VERSION = "1.2.11";
     public static final String DEPENDENCIES = "required-after:Forge@[12.18.2.2179,);required-after:mcmultipart@[1.2.1,);after:JEI@[3.13.2.348,);";
     public static final String GUI_FACTORY = "com.raoulvdberge.refinedstorage.gui.config.ModGuiFactory";
 
