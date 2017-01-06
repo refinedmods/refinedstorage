be2c134f81b848a9b63060f8ec49ab9b7c6a4893
way2muchnoise <jens.baetens@live.be>
2017-01-06 14:56:00 +0100

    revert use instance of in ItemValidatorBasic
    since it didn't work out

--- src/main/java/com/raoulvdberge/refinedstorage/inventory/ItemValidatorBasic.java
+++ src/main/java/com/raoulvdberge/refinedstorage/inventory/ItemValidatorBasic.java
@@ -18,7 +18,7 @@ public class ItemValidatorBasic implements IItemValidator {
 
     @Override
     public boolean isValid(ItemStack stack) {
-        if (item.getClass().isInstance(stack.getItem())) {
+        if (stack.getItem() == item) {
             if (damage != -1 && stack.getItemDamage() != damage) {
                 return false;
             }
