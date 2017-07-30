88b0d80383cf1b4ab4e9967cad833895a7e68f5e
raoulvdberge <raoulvdberge@gmail.com>
2017-07-31 00:40:25 +0200

    Respect NonNull for real real

--- src/main/java/com/raoulvdberge/refinedstorage/capability/CapabilityNetworkNodeProxy.java
+++ src/main/java/com/raoulvdberge/refinedstorage/capability/CapabilityNetworkNodeProxy.java
@@ -3,37 +3,49 @@ package com.raoulvdberge.refinedstorage.capability;
 import com.google.common.base.Preconditions;
 import com.raoulvdberge.refinedstorage.api.network.node.INetworkNode;
 import com.raoulvdberge.refinedstorage.api.network.node.INetworkNodeProxy;
-import com.raoulvdberge.refinedstorage.apiimpl.network.node.NetworkNodeDummy;
 import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.Callable;
 
 public class CapabilityNetworkNodeProxy {
     @CapabilityInject(INetworkNodeProxy.class)
     public static Capability<INetworkNodeProxy> NETWORK_NODE_PROXY_CAPABILITY = null;
 
     public static void register() {
-        CapabilityManager.INSTANCE.register(INetworkNodeProxy.class, new Capability.IStorage<INetworkNodeProxy>() {
-            public NBTBase writeNBT(Capability<INetworkNodeProxy> capability, INetworkNodeProxy instance, EnumFacing side) {
-                return new NBTTagCompound();
-            }
-
-            public void readNBT(Capability<INetworkNodeProxy> capability, INetworkNodeProxy instance, EnumFacing side, NBTBase base) {
-                // NO OP
-            }
-        }, () -> new INetworkNodeProxy() {
-            @Override
-            @Nonnull
-            public INetworkNode getNode() {
-                return new NetworkNodeDummy();
-            }
-        });
+        CapabilityManager.INSTANCE.register(INetworkNodeProxy.class, new Storage(), new Factory());
 
         Preconditions.checkNotNull(NETWORK_NODE_PROXY_CAPABILITY, "Capability not registered");
+    }
+
+    private static class Storage implements Capability.IStorage<INetworkNodeProxy> {
+        @Nullable
+        @Override
+        public NBTBase writeNBT(Capability<INetworkNodeProxy> capability, INetworkNodeProxy instance, EnumFacing side) {
+            return null;
+        }
+
+        @Override
+        public void readNBT(Capability<INetworkNodeProxy> capability, INetworkNodeProxy instance, EnumFacing side, NBTBase nbt) {
+            // NO OP
+        }
+    }
+
+    private static class Factory implements Callable<INetworkNodeProxy> {
+        @Override
+        public INetworkNodeProxy call() throws Exception {
+            return new INetworkNodeProxy() {
+                @Nonnull
+                @Override
+                public INetworkNode getNode() {
+                    throw new UnsupportedOperationException("Cannot use default implementation");
+                }
+            };
+        }
     }
 }
