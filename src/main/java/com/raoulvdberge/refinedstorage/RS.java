5d10b9554246ef680f23b7fd0e01399c93dd5686
raoulvdberge <raoulvdberge@gmail.com>
2017-12-10 23:03:38 +0100

    Bump to 1.5.28

--- src/main/java/com/raoulvdberge/refinedstorage/RS.java
+++ src/main/java/com/raoulvdberge/refinedstorage/RS.java
@@ -23,7 +23,7 @@ public final class RS {
     }
 
     public static final String ID = "refinedstorage";
-    public static final String VERSION = "1.5.27";
+    public static final String VERSION = "1.5.28";
     public static final String DEPENDENCIES = "required-after:forge@[14.21.0.2363,);after:mcmultipart@[2.2.1,);after:storagedrawers@[1.12-5.2.2,);";
     public static final String GUI_FACTORY = "com.raoulvdberge.refinedstorage.gui.config.ModGuiFactory";
     public static final String UPDATE_JSON = "https://refinedstorage.raoulvdberge.com/update";
