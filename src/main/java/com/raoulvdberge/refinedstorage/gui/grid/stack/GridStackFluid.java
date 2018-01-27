70fc62f16d860c68c46b09556e12e4cf2f70b6ed
ineternet <33813737+ineternet@users.noreply.github.com>
2018-01-27 23:23:01 +0100

    Changed to float display

--- src/main/java/com/raoulvdberge/refinedstorage/gui/grid/stack/GridStackFluid.java
+++ src/main/java/com/raoulvdberge/refinedstorage/gui/grid/stack/GridStackFluid.java
@@ -63,8 +63,9 @@ public class GridStackFluid implements IGridStack {
     public void draw(GuiBase gui, int x, int y) {
         GuiBase.FLUID_RENDERER.draw(gui.mc, x, y, stack);
 
-        int amount = (int) ((float) stack.amount / 1000F);
-        String formattedAmount = amount >= 1 ? API.instance().getQuantityFormatter().formatWithUnits(amount) : "<1";
+        float amountRaw = ((float) stack.amount / 1000F);
+        int amount = (int) amountRaw;
+        String formattedAmount = amount >= 1 ? API.instance().getQuantityFormatter().formatWithUnits(amount) : String.format("%.1f", amountRaw);
         
         gui.drawQuantity(x, y, formattedAmount);
     }
