3f360705f22f8fa25f05efd2b35d9a5bc3ae5103
raoulvdberge <raoulvdberge@gmail.com>
2018-06-01 15:43:07 +0200

    Remove useless check in external storage item handler.

--- src/main/java/com/raoulvdberge/refinedstorage/block/BlockWirelessTransmitter.java
+++ src/main/java/com/raoulvdberge/refinedstorage/block/BlockWirelessTransmitter.java
@@ -24,7 +24,6 @@ import java.util.List;
 
 public class BlockWirelessTransmitter extends BlockNode {
     // From BlockTorch
-    // TODO: make consistent with shape
     private static final AxisAlignedBB WIRELESS_TRANSMITTER_AABB = new AxisAlignedBB(0.4000000059604645D, 0.0D, 0.4000000059604645D, 0.6000000238418579D, 0.6000000238418579D, 0.6000000238418579D);
 
     public BlockWirelessTransmitter() {
