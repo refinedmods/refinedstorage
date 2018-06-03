9e9fb150bf7a0072741754904604ba7306198497
raoulvdberge <raoulvdberge@gmail.com>
2018-06-03 14:08:09 +0200

    Revert "Made item storage disks and blocks capacity a power of two (1k now means 1024 items instead of 1000, etc)."
    This reverts commit fd7a4d7

--- src/main/java/com/raoulvdberge/refinedstorage/block/ItemStorageType.java
+++ src/main/java/com/raoulvdberge/refinedstorage/block/ItemStorageType.java
@@ -3,10 +3,10 @@ package com.raoulvdberge.refinedstorage.block;
 import net.minecraft.util.IStringSerializable;
 
 public enum ItemStorageType implements IStringSerializable {
-    TYPE_1K(0, 1024, "1k"),
-    TYPE_4K(1, 4096, "4k"),
-    TYPE_16K(2, 16_384, "16k"),
-    TYPE_64K(3, 65_536, "64k"),
+    TYPE_1K(0, 1000, "1k"),
+    TYPE_4K(1, 4000, "4k"),
+    TYPE_16K(2, 16000, "16k"),
+    TYPE_64K(3, 64000, "64k"),
     TYPE_CREATIVE(4, -1, "creative");
 
     private int id;
