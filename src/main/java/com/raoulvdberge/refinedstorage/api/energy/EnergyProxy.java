7c214f22f130ea09788970e7cea6f51947b9c797
Samtrion <Samtrion@users.noreply.github.com>
2018-06-25 18:28:11 +0200

    Update EnergyProxy.java

--- src/main/java/com/raoulvdberge/refinedstorage/api/energy/EnergyProxy.java
+++ src/main/java/com/raoulvdberge/refinedstorage/api/energy/EnergyProxy.java
@@ -3,47 +3,47 @@ package com.raoulvdberge.refinedstorage.api.energy;
 import net.minecraftforge.energy.IEnergyStorage;
 
 public final class EnergyProxy implements IEnergyStorage {
-	private final int maxReceive;
-	private final int maxExtract;
-	private final IEnergy energy;
+    private final int maxReceive;
+    private final int maxExtract;
+    private final IEnergy energy;
 
-	public EnergyProxy(IEnergy energy, int maxTransfer) {
-		this(energy, maxTransfer, maxTransfer);
-	}
+    public EnergyProxy(IEnergy energy, int maxTransfer) {
+        this(energy, maxTransfer, maxTransfer);
+    }
 
-	public EnergyProxy(IEnergy energy, int maxReceive, int maxExtract) {
-		this.energy = energy;
-		this.maxReceive = maxReceive;
-		this.maxExtract = maxExtract;
-	}
+    public EnergyProxy(IEnergy energy, int maxReceive, int maxExtract) {
+        this.energy = energy;
+        this.maxReceive = maxReceive;
+        this.maxExtract = maxExtract;
+    }
 
-	@Override
-	public int receiveEnergy(int maxReceive, boolean simulate) {
-		return !canReceive() ? 0 : this.energy.insert(Math.min(this.maxReceive, maxReceive), simulate);
-	}
+    @Override
+    public int receiveEnergy(int maxReceive, boolean simulate) {
+        return !canReceive() ? 0 : this.energy.insert(Math.min(this.maxReceive, maxReceive), simulate);
+    }
 
-	@Override
-	public int extractEnergy(int maxExtract, boolean simulate) {
-		return !canExtract() ? 0 : this.energy.extract(Math.min(this.maxExtract, maxExtract), simulate);
-	}
+    @Override
+    public int extractEnergy(int maxExtract, boolean simulate) {
+        return !canExtract() ? 0 : this.energy.extract(Math.min(this.maxExtract, maxExtract), simulate);
+    }
 
-	@Override
-	public int getEnergyStored() {
-		return this.energy.getStored();
-	}
+    @Override
+    public int getEnergyStored() {
+        return this.energy.getStored();
+    }
 
-	@Override
-	public int getMaxEnergyStored() {
-		return this.energy.getCapacity();
-	}
+    @Override
+    public int getMaxEnergyStored() {
+        return this.energy.getCapacity();
+    }
 
-	@Override
-	public boolean canExtract() {
-		return this.maxExtract > 0;
-	}
+    @Override
+    public boolean canExtract() {
+        return this.maxExtract > 0;
+    }
 
-	@Override
-	public boolean canReceive() {
-		return this.maxReceive > 0;
-	}
+    @Override
+    public boolean canReceive() {
+        return this.maxReceive > 0;
+    }
 }
