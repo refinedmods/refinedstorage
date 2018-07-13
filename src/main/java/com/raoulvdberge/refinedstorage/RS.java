7cdeaf2c7812ea5bbedd9e1ee7da3a657b71e15e
raoulvdberge <raoulvdberge@gmail.com>
2018-07-13 21:23:43 +0200

    Include build number in version.

--- src/main/java/com/raoulvdberge/refinedstorage/RS.java
+++ src/main/java/com/raoulvdberge/refinedstorage/RS.java
@@ -23,7 +23,7 @@ public final class RS {
     }
 
     public static final String ID = "refinedstorage";
-    public static final String VERSION = "1.6";
+    public static final String VERSION = "@version@";
     public static final String GUI_FACTORY = "com.raoulvdberge.refinedstorage.gui.config.ModGuiFactory";
     public static final String UPDATE_JSON = "https://refinedstorage.raoulvdberge.com/update";
     public static final String FINGERPRINT = "57893d5b90a7336e8c63fe1c1e1ce472c3d59578";
