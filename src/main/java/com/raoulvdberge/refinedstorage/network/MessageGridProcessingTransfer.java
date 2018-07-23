c0ed630ac4c228d33ac16585384189d7dde022df
raoulvdberge <raoulvdberge@gmail.com>
2018-07-23 15:39:12 +0200

    Add JEI transfer autocompletion for fluids in Pattern Grid.

--- src/main/java/com/raoulvdberge/refinedstorage/network/MessageGridProcessingTransfer.java
+++ src/main/java/com/raoulvdberge/refinedstorage/network/MessageGridProcessingTransfer.java
@@ -5,10 +5,16 @@ import com.raoulvdberge.refinedstorage.api.network.grid.IGrid;
 import com.raoulvdberge.refinedstorage.apiimpl.network.node.NetworkNodeGrid;
 import com.raoulvdberge.refinedstorage.container.ContainerGrid;
 import com.raoulvdberge.refinedstorage.inventory.ItemHandlerBase;
+import com.raoulvdberge.refinedstorage.inventory.ItemHandlerFluid;
 import com.raoulvdberge.refinedstorage.util.StackUtils;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
 import java.util.ArrayList;
@@ -18,12 +24,17 @@ public class MessageGridProcessingTransfer extends MessageHandlerPlayerToServer<
     private Collection<ItemStack> inputs;
     private Collection<ItemStack> outputs;
 
+    private Collection<FluidStack> fluidInputs;
+    private Collection<FluidStack> fluidOutputs;
+
     public MessageGridProcessingTransfer() {
     }
 
-    public MessageGridProcessingTransfer(Collection<ItemStack> inputs, Collection<ItemStack> outputs) {
+    public MessageGridProcessingTransfer(Collection<ItemStack> inputs, Collection<ItemStack> outputs, Collection<FluidStack> fluidInputs, Collection<FluidStack> fluidOutputs) {
         this.inputs = inputs;
         this.outputs = outputs;
+        this.fluidInputs = fluidInputs;
+        this.fluidOutputs = fluidOutputs;
     }
 
     @Override
@@ -43,6 +54,26 @@ public class MessageGridProcessingTransfer extends MessageHandlerPlayerToServer<
         for (int i = 0; i < size; i++) {
             this.outputs.add(StackUtils.readItemStack(buf));
         }
+
+        size = buf.readInt();
+
+        this.fluidInputs = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            buf.readInt();
+
+            this.fluidInputs.add(StackUtils.readFluidStack(buf));
+        }
+
+        size = buf.readInt();
+
+        this.fluidOutputs = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            buf.readInt();
+
+            this.fluidOutputs.add(StackUtils.readFluidStack(buf));
+        }
     }
 
     @Override
@@ -58,6 +89,18 @@ public class MessageGridProcessingTransfer extends MessageHandlerPlayerToServer<
         for (ItemStack stack : outputs) {
             StackUtils.writeItemStack(buf, stack);
         }
+
+        buf.writeInt(fluidInputs.size());
+
+        for (FluidStack stack : fluidInputs) {
+            StackUtils.writeFluidStack(buf, stack);
+        }
+
+        buf.writeInt(fluidOutputs.size());
+
+        for (FluidStack stack : fluidOutputs) {
+            StackUtils.writeFluidStack(buf, stack);
+        }
     }
 
     @Override
@@ -67,11 +110,16 @@ public class MessageGridProcessingTransfer extends MessageHandlerPlayerToServer<
 
             if (grid.getGridType() == GridType.PATTERN) {
                 ItemHandlerBase handler = ((NetworkNodeGrid) grid).getProcessingMatrix();
+                ItemHandlerFluid handlerFluid = ((NetworkNodeGrid) grid).getMatrixProcessingFluids();
 
                 clearInputsAndOutputs(handler);
+                clearInputsAndOutputs(handlerFluid);
 
                 setInputs(handler, message.inputs);
                 setOutputs(handler, message.outputs);
+
+                setFluidInputs(handlerFluid, message.fluidInputs);
+                setFluidOutputs(handlerFluid, message.fluidOutputs);
             }
         }
     }
@@ -93,6 +141,39 @@ public class MessageGridProcessingTransfer extends MessageHandlerPlayerToServer<
     private void setSlots(ItemHandlerBase handler, Collection<ItemStack> stacks, int begin, int end) {
         for (ItemStack stack : stacks) {
             handler.setStackInSlot(begin, stack);
+
+            begin++;
+
+            if (begin >= end) {
+                break;
+            }
+        }
+    }
+
+    private void setFluidInputs(ItemHandlerBase handler, Collection<FluidStack> stacks) {
+        setFluidSlots(handler, stacks, 0, 9);
+    }
+
+    private void setFluidOutputs(ItemHandlerBase handler, Collection<FluidStack> stacks) {
+        setFluidSlots(handler, stacks, 9, 18);
+    }
+
+    private void setFluidSlots(ItemHandlerBase handler, Collection<FluidStack> stacks, int begin, int end) {
+        for (FluidStack stack : stacks) {
+            if (!StackUtils.hasFluidBucket(stack) || stack.amount > Fluid.BUCKET_VOLUME) {
+                continue;
+            }
+
+            ItemStack filledContainer = new ItemStack(Items.BUCKET);
+
+            IFluidHandlerItem fluidHandler = filledContainer.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+
+            fluidHandler.fill(StackUtils.copy(stack, Fluid.BUCKET_VOLUME), true);
+
+            filledContainer = fluidHandler.getContainer();
+            filledContainer.setCount(stack.amount);
+
+            handler.setStackInSlot(begin, filledContainer);
 
             begin++;
 
