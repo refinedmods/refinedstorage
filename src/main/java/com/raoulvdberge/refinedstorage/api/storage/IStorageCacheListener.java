0e1b753289d70a93f06c8de6bf174eb26ae1d9b2
Mike Caron <caron.mike@gmail.com>
2018-09-07 12:33:27 -0400

    Add a new method to IStorageCacheListener for bulk operations

--- src/main/java/com/raoulvdberge/refinedstorage/api/storage/IStorageCacheListener.java
+++ src/main/java/com/raoulvdberge/refinedstorage/api/storage/IStorageCacheListener.java
@@ -1,6 +1,10 @@
 package com.raoulvdberge.refinedstorage.api.storage;
 
+import net.minecraft.item.ItemStack;
+import org.apache.commons.lang3.tuple.Pair;
+
 import javax.annotation.Nonnull;
+import java.util.List;
 
 /**
  * Listens for storage cache changes.
@@ -25,4 +29,10 @@ public interface IStorageCacheListener<T> {
      * @param size  the size, negative if the amount decreases
      */
     void onChanged(@Nonnull T stack, int size);
+
+    default void onChangedBulk(@Nonnull List<Pair<T, Integer>> stacks)
+    {
+        for(Pair<T, Integer> stack : stacks)
+            onChanged(stack.getLeft(), stack.getRight());
+    }
 }
