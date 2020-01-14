10c9dbbe075eb1e584dec88b3c78f71c84a8708f
raoulvdberge <raoulvdberge@gmail.com>
2020-01-14 12:55:21 +0100

    Fix pattern item stack tile renderer

--- src/main/java/com/raoulvdberge/refinedstorage/render/tesr/PatternItemStackTileRenderer.java
+++ src/main/java/com/raoulvdberge/refinedstorage/render/tesr/PatternItemStackTileRenderer.java
@@ -1,17 +1,19 @@
 package com.raoulvdberge.refinedstorage.render.tesr;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.raoulvdberge.refinedstorage.apiimpl.autocrafting.CraftingPattern;
 import com.raoulvdberge.refinedstorage.item.PatternItem;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.item.ItemStack;
 
 public class PatternItemStackTileRenderer extends ItemStackTileEntityRenderer {
-    /* TODO @Override
-    public void renderByItem(ItemStack stack) {
+    @Override
+    public void func_228364_a_(ItemStack stack, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int p_228364_4_, int p_228364_5_) {
         CraftingPattern pattern = PatternItem.fromCache(null, stack);
 
         ItemStack outputStack = pattern.getOutputs().get(0);
 
-        outputStack.getItem().getTileEntityItemStackRenderer().renderByItem(outputStack);
-    }*/
+        outputStack.getItem().getTileEntityItemStackRenderer().func_228364_a_(outputStack, matrixStack, renderTypeBuffer, p_228364_4_, p_228364_5_);
+    }
 }
