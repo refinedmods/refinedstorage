185f3c06c825ccb046d475517e6908da9398163b
Darkere <Dark-277@web.de>
2020-05-31 20:57:01 +0200

    rewrite JEI show missing and craftable items so that: - clicking transfer button still works - it is also shown for processing recipes - also for fluids

--- src/main/java/com/refinedmods/refinedstorage/integration/jei/GridRecipeTransferHandler.java
+++ src/main/java/com/refinedmods/refinedstorage/integration/jei/GridRecipeTransferHandler.java
@@ -10,6 +10,7 @@ import com.refinedmods.refinedstorage.network.grid.GridProcessingTransferMessage
 import com.refinedmods.refinedstorage.network.grid.GridTransferMessage;
 import com.refinedmods.refinedstorage.screen.IScreenInfoProvider;
 import com.refinedmods.refinedstorage.screen.grid.GridScreen;
+import com.refinedmods.refinedstorage.screen.grid.stack.FluidGridStack;
 import com.refinedmods.refinedstorage.screen.grid.stack.IGridStack;
 import com.refinedmods.refinedstorage.screen.grid.stack.ItemGridStack;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
@@ -19,11 +20,9 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.transfer.IRecipeTransferError;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.container.Container;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -47,163 +46,6 @@ public class GridRecipeTransferHandler implements IRecipeTransferHandler {
     @Override
     public IRecipeTransferError transferRecipe(Container container, IRecipeLayout recipeLayout, PlayerEntity player, boolean maxTransfer, boolean doTransfer) {
         IGrid grid = ((GridContainer) container).getGrid();
-        // Check whether we have enough items to craft the recipe and return an error if we don't.
-        // To get the items grid, we get the grid's GUI from an internal JEI method and use its GridView.
-        // There is no other (synchronous) way of retrieving a list of items in the grid.
-
-        // This should try to replicate the behaviour of NetworkNodeGrid#onRecipeTransfer.
-        // Assume there is enough space in the network to clear the crafting matrix - there's no way of determining
-        // this without access to the network itself.
-        if (isCraftingRecipe(recipeLayout.getRecipeCategory())) {
-            IScreenInfoProvider parentScreen = ((GridContainer) container).getScreenInfoProvider();
-            if (parentScreen instanceof GridScreen) {
-
-                // Using IGridView#getStacks will return a *filtered* list of items in the view,
-                // which will cause problems - especially if the user uses JEI synchronised searching.
-                // Instead, we will use IGridView#getAllStacks which provides an unordered view of all GridStacks.
-                Collection<IGridStack> gridStacks = ((GridScreen) parentScreen).getView().getAllStacks();
-
-                boolean[] isFound = new boolean[9];
-                boolean[] canAutocraft = new boolean[9];
-                // Map from Items to a 9-long array of slots, where each slot is an array of ItemStacks which is
-                // valid in that slot of the recipe.
-                Map<Item, List<List<ItemStack>>> ingredientsToSlotsToStacks = new HashMap<>();
-
-                Map<Integer, ? extends IGuiIngredient<ItemStack>> guiIngredients = recipeLayout.getItemStacks().getGuiIngredients();
-
-                // Initialise variables
-                for (int i = 0; i < 9; i++) {
-                    isFound[i] = true;
-                    List<ItemStack> ingredients = guiIngredients.get(i + 1).getAllIngredients();
-                    for (ItemStack ingredient : ingredients) {
-                        if (ingredient == null) {
-                            continue;
-                        }
-                        isFound[i] = false;
-                        Item item = ingredient.getItem();
-                        if (!ingredientsToSlotsToStacks.containsKey(item)) {
-                            List<List<ItemStack>> slots = new ArrayList<>(9);
-                            for (int j = 0; j < 9; j++) {
-                                slots.add(new ArrayList<>());
-                            }
-                            ingredientsToSlotsToStacks.put(item, slots);
-                        }
-                        ingredientsToSlotsToStacks.get(item).get(i).add(ingredient);
-                    }
-                }
-
-                // Check grid
-                IComparer comparer = API.instance().getComparer();
-
-                for (IGridStack gridStack : gridStacks) {
-                    if (!(gridStack instanceof ItemGridStack)) {
-                        continue;
-                    }
-                    if (!ingredientsToSlotsToStacks.containsKey(((ItemGridStack) gridStack).getStack().getItem())) {
-                        continue;
-                    }
-                    ItemGridStack gridStackItem = (ItemGridStack) gridStack;
-                    ItemStack stack = gridStackItem.getStack();
-                    Item item = stack.getItem();
-                    List<List<ItemStack>> slots = ingredientsToSlotsToStacks.get(item);
-
-                    int available = gridStackItem.getQuantity();
-                    for (int i = 0; i < 9; i++) {
-                        if (isFound[i]) {
-                            continue;
-                        }
-                        for (ItemStack possibility : slots.get(i)) {
-                            if (comparer.isEqual(possibility, stack, IComparer.COMPARE_NBT)) {
-                                if (possibility.getCount() <= available) {
-                                    isFound[i] = true;
-                                    available -= possibility.getCount();
-                                    break;
-                                } else if (gridStackItem.isCraftable()) {
-                                    canAutocraft[i] = true;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                // Check inventory
-                for (int inventorySlot = 0; inventorySlot < player.inventory.getSizeInventory(); inventorySlot++) {
-                    if (!ingredientsToSlotsToStacks.containsKey(player.inventory.getStackInSlot(inventorySlot).getItem())) {
-                        continue;
-                    }
-                    ItemStack stack = player.inventory.getStackInSlot(inventorySlot);
-                    Item item = stack.getItem();
-                    List<List<ItemStack>> slots = ingredientsToSlotsToStacks.get(item);
-
-                    int available = stack.getCount();
-                    for (int i = 0; i < 9; i++) {
-                        if (isFound[i]) {
-                            continue;
-                        }
-                        for (ItemStack possibility : slots.get(i)) {
-                            if (comparer.isEqual(possibility, stack, IComparer.COMPARE_NBT)) {
-                                if (possibility.getCount() <= available) {
-                                    isFound[i] = true;
-                                    available -= possibility.getCount();
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                // Check grid crafting slots
-                CraftingInventory craftingMatrix = grid.getCraftingMatrix();
-                if (craftingMatrix != null) {
-                    for (int matrixSlot = 0; matrixSlot < craftingMatrix.getSizeInventory(); matrixSlot++) {
-                        if (!ingredientsToSlotsToStacks.containsKey(craftingMatrix.getStackInSlot(matrixSlot).getItem())) {
-                            continue;
-                        }
-                        ItemStack stack = craftingMatrix.getStackInSlot(matrixSlot);
-
-                        Item item = stack.getItem();
-                        List<List<ItemStack>> slots = ingredientsToSlotsToStacks.get(item);
-
-                        int available = stack.getCount();
-                        for (int i = 0; i < 9; i++) {
-                            if (isFound[i]) {
-                                continue;
-                            }
-                            for (ItemStack possibility : slots.get(i)) {
-                                if (comparer.isEqual(possibility, stack, IComparer.COMPARE_NBT)) {
-                                    if (possibility.getCount() <= available) {
-                                        isFound[i] = true;
-                                        available -= possibility.getCount();
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                // Generate an appropriate error if necessary
-                List<Integer> missingItems = new ArrayList<>();
-                List<Integer> missingAutocraftItems = new ArrayList<>();
-                for (int i = 0; i < 9; i++) {
-                    if (!isFound[i]) {
-                        if (canAutocraft[i]) {
-                            missingAutocraftItems.add(i + 1);
-                        } else {
-                            missingItems.add(i + 1);
-                        }
-                    }
-                }
-                if (!missingAutocraftItems.isEmpty()) {
-                    return new com.refinedmods.refinedstorage.integration.jei.RecipeTransferErrorAutocraftSlots(
-                        I18n.format("jei.tooltip.error.recipe.transfer.missing"),
-                        I18n.format("gui.refinedstorage:jei.tooltip.error.recipe.transfer.missing.autocraft"),
-                        missingItems,
-                        missingAutocraftItems);
-                } else if (!missingItems.isEmpty()) {
-                    return this.transferHelper.createUserErrorForSlots(I18n.format("jei.tooltip.error.recipe.transfer.missing"), missingItems);
-                }
-            }
-        }
-
         if (doTransfer) {
             LAST_TRANSFER_TIME = System.currentTimeMillis();
 
@@ -245,6 +87,65 @@ public class GridRecipeTransferHandler implements IRecipeTransferHandler {
                     container.inventorySlots.stream().filter(s -> s.inventory instanceof CraftingInventory).collect(Collectors.toList())
                 ));
             }
+
+            return null;
+        }
+
+        // Check whether we have enough items to craft the recipe and return an error if we don't.
+        // To get the items grid, we get the grid's GUI from an internal JEI method and use its GridView.
+        // There is no other (synchronous) way of retrieving a list of items in the grid.
+        IScreenInfoProvider gridSreen = ((GridContainer) container).getScreenInfoProvider();
+        if (gridSreen instanceof GridScreen) {
+            // Using IGridView#getStacks will return a *filtered* list of items in the view,
+            // which will cause problems - especially if the user uses JEI synchronised searching.
+            // Instead, we will use IGridView#getAllStacks which provides an unordered view of all GridStacks.
+            Collection<IGridStack> gridStacks = ((GridScreen) gridSreen).getView().getAllStacks();
+            List<GuiIngredientAvailability> ingredients = new ArrayList<>();
+            List<GuiIngredientAvailability> ingredientsFluids = new ArrayList<>();
+
+            //Initialise ingredients
+            for (Map.Entry<Integer, ? extends IGuiIngredient<ItemStack>> entry : recipeLayout.getItemStacks().getGuiIngredients().entrySet()) {
+                if (entry.getValue().isInput() && !entry.getValue().getAllIngredients().isEmpty()) {
+                    ingredients.add(new GuiIngredientAvailability<>(entry.getValue()));
+                }
+            }
+            for (Map.Entry<Integer, ? extends IGuiIngredient<FluidStack>> entry : recipeLayout.getFluidStacks().getGuiIngredients().entrySet()) {
+                if (entry.getValue().isInput() && !entry.getValue().getAllIngredients().isEmpty()) {
+                    ingredientsFluids.add(new GuiIngredientAvailability<>(entry.getValue()));
+                }
+            }
+
+            // Check grid
+            for (IGridStack gridStack : gridStacks) {
+                if ((gridStack instanceof ItemGridStack)) {
+                    checkIfStackIsNeeded(ingredients, ((ItemGridStack) gridStack).getStack(), gridStack.isCraftable());
+                } else if (gridStack instanceof FluidGridStack) {
+                    checkIfStackIsNeeded(ingredientsFluids, ((FluidGridStack) gridStack).getStack(), gridStack.isCraftable());
+                }
+
+            }
+
+            // Check inventory
+            for (int inventorySlot = 0; inventorySlot < player.inventory.getSizeInventory(); inventorySlot++) {
+                if(!player.inventory.getStackInSlot(inventorySlot).isEmpty()){
+                    checkIfStackIsNeeded(ingredients, player.inventory.getStackInSlot(inventorySlot), false);
+                }
+            }
+
+            // Check grid crafting slots
+            if (grid.getGridType().equals(GridType.CRAFTING)) {
+                CraftingInventory craftingMatrix = grid.getCraftingMatrix();
+                if (craftingMatrix != null) {
+                    for (int matrixSlot = 0; matrixSlot < craftingMatrix.getSizeInventory(); matrixSlot++) {
+                        if(!craftingMatrix.getStackInSlot(matrixSlot).isEmpty()){
+                            checkIfStackIsNeeded(ingredients, craftingMatrix.getStackInSlot(matrixSlot), false);
+                        }
+                    }
+                }
+            }
+            if (ingredients.stream().anyMatch(x -> !x.isAvailable())||ingredientsFluids.stream().anyMatch(x->!x.isAvailable())) {
+                return new RecipeTransferGridError(grid.getGridType(), ingredients, ingredientsFluids);
+            }
         }
 
         return null;
@@ -252,5 +153,68 @@ public class GridRecipeTransferHandler implements IRecipeTransferHandler {
 
     private boolean isCraftingRecipe(IRecipeCategory<?> recipeCategory) {
         return recipeCategory.getUid().equals(VanillaRecipeCategoryUid.CRAFTING);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private void checkIfStackIsNeeded(List<GuiIngredientAvailability> ingredients, Object stack, boolean isCraftable) {
+        int available;
+        boolean isItem = stack instanceof ItemStack;
+        if (stack instanceof ItemStack) {
+            available = ((ItemStack) stack).getCount();
+        } else if (stack instanceof FluidStack) {
+            available = ((FluidStack) stack).getAmount();
+        } else {
+            return;
+        }
+        for (GuiIngredientAvailability GIA : ingredients) {
+            if(available == 0){
+                return;
+            }
+            if (GIA.isAvailable()) {
+                continue;
+            }
+            Optional match = GIA.guiIngredient.getAllIngredients().stream().filter(x -> {
+                if (isItem) {
+                    return API.instance().getComparer().isEqual((ItemStack) stack, (ItemStack) x, IComparer.COMPARE_NBT);
+                } else {
+                    return API.instance().getComparer().isEqual((FluidStack) stack, (FluidStack) x, IComparer.COMPARE_NBT);
+                }
+            }).findFirst();
+            if (!match.isPresent()) {
+                continue;
+            } else if (isCraftable) {
+                GIA.isCraftable = true;
+            } else { //crafables and non craftables are 2 different gridstacks
+                int needed = Math.min(GIA.required - GIA.fulfilled, Math.min(available, isItem ? ((ItemStack) match.get()).getCount() : ((FluidStack) match.get()).getAmount()));
+                GIA.fulfilled += needed;
+                available -= needed;
+            }
+        }
+    }
+
+    public static class GuiIngredientAvailability<T> {
+        IGuiIngredient<T> guiIngredient;
+        boolean isCraftable;
+        int required;
+        int fulfilled;
+
+        public GuiIngredientAvailability(IGuiIngredient<T> guiIngredient) {
+            this.guiIngredient = guiIngredient;
+            Object stack;
+            stack = guiIngredient.getAllIngredients().get(0);
+            if (stack instanceof ItemStack) {
+                required = ((ItemStack) stack).getCount();
+            } else if (stack instanceof FluidStack) {
+                required = ((FluidStack) stack).getAmount();
+            }
+        }
+
+        public boolean isAvailable() {
+            return required == fulfilled;
+        }
+
+        public boolean isCraftable() {
+            return isCraftable;
+        }
     }
 }
