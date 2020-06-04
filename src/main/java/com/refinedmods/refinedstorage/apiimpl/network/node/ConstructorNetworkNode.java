be66516dc29f54736acbc84733e18705238c3921
Darkere <Dark-277@web.de>
2020-06-04 19:05:11 +0200

    fix constructor extracting 2B from network upon placing fixes #2523

--- src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/ConstructorNetworkNode.java
+++ src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/ConstructorNetworkNode.java
@@ -104,7 +104,6 @@ public class ConstructorNetworkNode extends NetworkNode implements IComparable, 
             }
         } else if (world.isAirBlock(front) && FluidUtil.tryPlaceFluid(WorldUtils.getFakePlayer((ServerWorld) world, getOwner()), world, Hand.MAIN_HAND, front, new NetworkFluidHandler(StackUtils.copy(stack, FluidAttributes.BUCKET_VOLUME)), stack)) {
             // We manually have to check world.isAirBlock in the else if statement because tryPlaceFluid ignores this.
-            network.extractFluid(stack, FluidAttributes.BUCKET_VOLUME, Action.PERFORM);
         }
     }
 
