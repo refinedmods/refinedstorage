a6d7ce87d97babdd928baab2e6381a8914c2afa5
Anton Bulakh <self@necauqua.dev>
2021-06-11 23:32:57 +0300

    Fix pattern cache

--- src/main/java/com/refinedmods/refinedstorage/item/PatternItem.java
+++ src/main/java/com/refinedmods/refinedstorage/item/PatternItem.java
@@ -35,7 +35,25 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class PatternItem extends Item implements ICraftingPatternProvider {
-    private static final Map<ItemStack, ICraftingPattern> CACHE = new HashMap<>();
+    private static final Map<StackCacheKey, ICraftingPattern> CACHE = new HashMap<>();
+
+    private static final class StackCacheKey {
+        public final ItemStack stack;
+
+        private StackCacheKey(ItemStack stack) {
+            this.stack = stack;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return this == o || o != null && getClass() == o.getClass() && ItemStack.areItemStacksEqual(stack, ((StackCacheKey) o).stack);
+        }
+
+        @Override
+        public int hashCode() {
+            return 961 * stack.getCount() + 31 * stack.getItem().hashCode() + (stack.hasTag() ? stack.getTag().hashCode() : 0);
+        }
+    }
 
     private static final String NBT_VERSION = "Version";
     private static final String NBT_INPUT_SLOT = "Input_%d";
@@ -53,11 +71,18 @@ public class PatternItem extends Item implements ICraftingPatternProvider {
     }
 
     public static ICraftingPattern fromCache(World world, ItemStack stack) {
-        if (!CACHE.containsKey(stack)) {
-            CACHE.put(stack, CraftingPatternFactory.INSTANCE.create(world, null, stack));
+        ICraftingPattern pattern = CACHE.computeIfAbsent(
+            new StackCacheKey(stack),
+            s -> CraftingPatternFactory.INSTANCE.create(world, null, s.stack)
+        );
+
+        // A number that is not too crazy but hopefully is not normally reachable,
+        // just reset the cache to keep its size limited so this is not a memory leak
+        if (CACHE.size() > 16384) {
+            CACHE.clear();
         }
 
-        return CACHE.get(stack);
+        return pattern;
     }
 
     @Override
