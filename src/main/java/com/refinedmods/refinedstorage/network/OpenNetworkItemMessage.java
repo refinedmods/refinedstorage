53692239601d0cbfc3efce4bf2d7f4568a1d7206
Darkere <Dark-277@web.de>
2021-06-13 00:30:34 +0200

    add curio support for wireless items

--- src/main/java/com/refinedmods/refinedstorage/network/OpenNetworkItemMessage.java
+++ src/main/java/com/refinedmods/refinedstorage/network/OpenNetworkItemMessage.java
@@ -4,26 +4,35 @@ import com.refinedmods.refinedstorage.apiimpl.API;
 import com.refinedmods.refinedstorage.apiimpl.network.grid.factory.PortableGridGridFactory;
 import com.refinedmods.refinedstorage.item.NetworkItem;
 import com.refinedmods.refinedstorage.item.blockitem.PortableGridBlockItem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fml.network.NetworkEvent;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class OpenNetworkItemMessage {
     private final int slotId;
+    private final String curioSlot;
 
-    public OpenNetworkItemMessage(int slotId) {
+    public OpenNetworkItemMessage(int slotId, String curioSlot) {
         this.slotId = slotId;
+        this.curioSlot = curioSlot;
     }
 
     public static OpenNetworkItemMessage decode(PacketBuffer buf) {
-        return new OpenNetworkItemMessage(buf.readInt());
+        return new OpenNetworkItemMessage(buf.readInt(), buf.readString(32767));
     }
 
     public static void encode(OpenNetworkItemMessage message, PacketBuffer buf) {
         buf.writeInt(message.slotId);
+        buf.writeString(message.curioSlot);
     }
 
     public static void handle(OpenNetworkItemMessage message, Supplier<NetworkEvent.Context> ctx) {
@@ -31,10 +40,26 @@ public class OpenNetworkItemMessage {
 
         if (player != null) {
             ctx.get().enqueueWork(() -> {
-                ItemStack stack = player.inventory.getStackInSlot(message.slotId);
+                ItemStack stack = null;
+                if (message.curioSlot.isEmpty()) {
+                    stack = player.inventory.getStackInSlot(message.slotId);
+                } else {
+                    LazyOptional<ICuriosItemHandler> curiosHandler = CuriosApi.getCuriosHelper().getCuriosHandler(player);
+                    if (curiosHandler.isPresent()) {
+                        Optional<ICurioStacksHandler> stacksHandler = curiosHandler.resolve().get().getStacksHandler(message.curioSlot);
+                        if (stacksHandler.isPresent()) {
+                            stack = stacksHandler.get().getStacks().getStackInSlot(message.slotId);
+                        }
+                    }
+                }
+
+                if (stack == null) {
+                    return;
+                }
 
                 if (stack.getItem() instanceof NetworkItem) {
-                    ((NetworkItem) stack.getItem()).applyNetwork(player.getServer(), stack, n -> n.getNetworkItemManager().open(player, stack, message.slotId), err -> player.sendMessage(err, player.getUniqueID()));
+                    ItemStack finalStack = stack;
+                    ((NetworkItem) stack.getItem()).applyNetwork(player.getServer(), stack, n -> n.getNetworkItemManager().open(player, finalStack, message.slotId), err -> player.sendMessage(err, player.getUniqueID()));
                 } else if (stack.getItem() instanceof PortableGridBlockItem) {
                     API.instance().getGridManager().openGrid(PortableGridGridFactory.ID, player, stack, message.slotId);
                 }
