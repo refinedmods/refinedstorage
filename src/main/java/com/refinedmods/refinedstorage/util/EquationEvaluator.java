b9cb5ba6f5ce04ec7cbe91aab243070af33dc544
ylou <1237500+ylou@users.noreply.github.com>
2021-06-13 18:37:38 -0700

    Add support for implicit multiply in equations

--- src/main/java/com/refinedmods/refinedstorage/util/EquationEvaluator.java
+++ src/main/java/com/refinedmods/refinedstorage/util/EquationEvaluator.java
@@ -6,6 +6,8 @@ import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 import java.util.function.Predicate;
 
+import javax.annotation.Nullable;
+
 /**
  * Implements evaluation for simple math expressions using the shunting yard
  * algorithm.
@@ -13,47 +15,62 @@ import java.util.function.Predicate;
 public class EquationEvaluator {
 
     public static double evaluate(String input) {
-        StringTokenizer tokens = new StringTokenizer(input, "+-*/()", true);
-        Deque<String> operators = new ArrayDeque<>();
-        Deque<Double> operands = new ArrayDeque<>();
+        final StringTokenizer tokens = new StringTokenizer(input, "+-*/()", true);
+        final Deque<String> operators = new ArrayDeque<>();
+        final Deque<Double> operands = new ArrayDeque<>();
         String lastToken = null;
         while (tokens.hasMoreTokens()) {
             String rawToken = tokens.nextToken().trim();
             if (rawToken.isEmpty())
                 continue;
-
-            boolean isUnaryMinus = rawToken.equals("-") && (lastToken == null
-                    || (!lastToken.equals("u") && isOperator(lastToken)) || lastToken.equals("("));
-            final String token = isUnaryMinus ? "u" : rawToken;
-            if (isOperator(token)) {
-                popOperators(input, operators, operands,
-                        top -> (!top.equals("(") && getPrecedence(token) <= getPrecedence(top)));
-                operators.push(token);
-            } else if (token.equals("(")) {
-                operators.push(token);
-            } else if (token.equals(")")) {
-                popOperators(input, operators, operands, top -> !top.equals("("));
-                if (!operators.pop().equals("(")) {
-                    throw new IllegalArgumentException("Unbalanced right parentheses in expression: " + input);
-                }
-            } else {
-                try {
-                    operands.push(Double.parseDouble(token));
-                } catch (NumberFormatException exception) {
-                    throw new IllegalArgumentException("Could not evaluate expression: " + input);
-                }
+            if (isUnaryMinus(rawToken, lastToken)) {
+                rawToken = "u";
+            } else if (isImplicitMultiply(rawToken, lastToken)) {
+                lastToken = processToken(input, "*", operators, operands);
             }
-            lastToken = token;
+            lastToken = processToken(input, rawToken, operators, operands);
         }
         popOperators(input, operators, operands, top -> true);
         return operands.pop();
+    }
+
+    private static boolean isUnaryMinus(String rawToken, @Nullable String lastToken) {
+        return rawToken.equals("-")
+                && (lastToken == null || (isOperator(lastToken) && !lastToken.equals("u")) || lastToken.equals("("));
+    }
+
+    private static boolean isImplicitMultiply(String rawToken, @Nullable String lastToken) {
+        return rawToken.equals("(")
+                && (lastToken != null && !isOperator(lastToken) && !lastToken.equals("(") && !lastToken.equals(")"));
+    }
+
+    private static String processToken(String input, String token, Deque<String> operators, Deque<Double> operands) {
+        if (isOperator(token)) {
+            popOperators(input, operators, operands,
+                    top -> (!top.equals("(") && getPrecedence(token) <= getPrecedence(top)));
+            operators.push(token);
+        } else if (token.equals("(")) {
+            operators.push(token);
+        } else if (token.equals(")")) {
+            popOperators(input, operators, operands, top -> !top.equals("("));
+            if (!operators.pop().equals("(")) {
+                throw new IllegalArgumentException("Unbalanced right parentheses in expression: " + input);
+            }
+        } else {
+            try {
+                operands.push(Double.parseDouble(token));
+            } catch (NumberFormatException exception) {
+                throw new IllegalArgumentException("Could not parse double from:" + token);
+            }
+        }
+        return token;
     }
 
     private static void popOperators(String input, Deque<String> operators, Deque<Double> operands,
             Predicate<String> predicate) {
         try {
             while (!operators.isEmpty() && predicate.test(operators.peek())) {
-                String op = operators.pop();
+                final String op = operators.pop();
                 if (op.equals("u")) {
                     operands.push(-operands.pop());
                     continue;
@@ -79,8 +96,8 @@ public class EquationEvaluator {
         throw new IllegalArgumentException("Unexpected arithmetic operation " + op);
     }
 
-    private static boolean isOperator(String op) {
-        switch (op) {
+    private static boolean isOperator(String token) {
+        switch (token) {
             case "+":
             case "-":
             case "*":
