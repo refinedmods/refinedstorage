82ad38b085692138712f0c70d404f0ef0aee52db
Darkere <Dark-277@web.de>
2021-12-04 16:31:20 +0100

    revert accidental change

--- src/main/java/com/refinedmods/refinedstorage/screen/widget/sidebutton/TypeSideButton.java
+++ src/main/java/com/refinedmods/refinedstorage/screen/widget/sidebutton/TypeSideButton.java
@@ -2,6 +2,8 @@ package com.refinedmods.refinedstorage.screen.widget.sidebutton;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.refinedmods.refinedstorage.screen.BaseScreen;
+import com.refinedmods.refinedstorage.tile.config.IType;
+import com.refinedmods.refinedstorage.tile.data.TileDataManager;
 import com.refinedmods.refinedstorage.tile.data.TileDataParameter;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.TextFormatting;
@@ -27,6 +29,6 @@ public class TypeSideButton extends SideButton {
 
     @Override
     public void onPress() {
-
+        TileDataManager.setParameter(type, type.getValue() == IType.ITEMS ? IType.FLUIDS : IType.ITEMS);
     }
 }
