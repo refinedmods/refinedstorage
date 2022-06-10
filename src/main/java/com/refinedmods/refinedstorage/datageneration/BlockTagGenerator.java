1422dc0d3fe62307074495417dd15beb302f5482
aeonic <ericknwash@gmail.com>
2022-06-10 18:26:18 -0500

    Add non-colored blocks to Forge relocation blacklist tag

--- src/main/java/com/refinedmods/refinedstorage/datageneration/BlockTagGenerator.java
+++ src/main/java/com/refinedmods/refinedstorage/datageneration/BlockTagGenerator.java
@@ -1,13 +1,10 @@
 package com.refinedmods.refinedstorage.datageneration;
 
 import com.refinedmods.refinedstorage.RSBlocks;
-import com.refinedmods.refinedstorage.RSItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
-import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -25,5 +22,9 @@ public class BlockTagGenerator extends BlockTagsProvider {
             map.values().forEach(block -> tag(tag).add(block.get()));
             noRelocationTag.addTags(tag);
         });
+        RSBlocks.STORAGE_BLOCKS.forEach((tag, block) -> noRelocationTag.add(block.get()));
+        RSBlocks.FLUID_STORAGE_BLOCKS.forEach((tag, block) -> noRelocationTag.add(block.get()));
+
+        noRelocationTag.add(RSBlocks.IMPORTER.get(), RSBlocks.EXPORTER.get(), RSBlocks.EXTERNAL_STORAGE.get(), RSBlocks.DISK_DRIVE.get(), RSBlocks.INTERFACE.get(), RSBlocks.FLUID_INTERFACE.get(), RSBlocks.STORAGE_MONITOR.get(), RSBlocks.CONSTRUCTOR.get(), RSBlocks.DESTRUCTOR.get(), RSBlocks.PORTABLE_GRID.get(), RSBlocks.CREATIVE_PORTABLE_GRID.get());
     }
 }
