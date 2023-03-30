refinedmods/refinedstorage

commit 045b90a463298c273f9775d5d21aed1a8a55c02e

Fixed a bunch of issues where chunks would unintentionally be loaded by RS. Fixes part of #3468

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/NetworkNode.java
@@ -237,12 +237,16 @@ public abstract class NetworkNode implements INetworkNode, INetworkNodeVisitor {
     @Override
     public void visit(Operator operator) {
         for (Direction facing : Direction.values()) {
-            INetworkNode oppositeNode = NetworkUtils.getNodeFromBlockEntity(level.getBlockEntity(pos.relative(facing)));
+            BlockPos facingPos = pos.relative(facing);
+            if (!level.isLoaded(facingPos)) {
+                return;
+            }
+            INetworkNode oppositeNode = NetworkUtils.getNodeFromBlockEntity(level.getBlockEntity(facingPos));
             if (oppositeNode == null) {
                 continue;
             }
             if (canConduct(facing) && oppositeNode.canReceive(facing.getOpposite())) {
-                operator.apply(level, pos.relative(facing), facing.getOpposite());
+                operator.apply(level, facingPos, facing.getOpposite());
             }
         }
     }

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/Network.java
@@ -101,8 +101,10 @@ public class Network implements INetwork, IRedstoneConfigurable {
         this.type = type;
         this.root = new RootNetworkNode(this, level, pos);
         this.nodeGraph.addListener(() -> {
+            if (!level.isLoaded(pos)) {
+                return;
+            }
             BlockEntity blockEntity = level.getBlockEntity(pos);
-
             if (blockEntity instanceof ControllerBlockEntity) {
                 ((ControllerBlockEntity) blockEntity).getDataManager().sendParameterToWatchers(ControllerBlockEntity.NODES);
             }

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/NetworkNodeGraph.java
@@ -118,9 +118,11 @@ public class NetworkNodeGraph implements INetworkNodeGraph {
                 for (Direction checkSide : Direction.values()) {
                     if (checkSide != side) { // Avoid going backward
                         INetworkNode nodeOnSide = NetworkUtils.getNodeFromBlockEntity(blockEntity);
-
                         if (nodeOnSide == node) {
-                            operator.apply(level, pos.relative(checkSide), checkSide.getOpposite());
+                            BlockPos relativePos = pos.relative(checkSide);
+                            if (level.isLoaded(relativePos)) {
+                                operator.apply(level, relativePos, checkSide.getOpposite());
+                            }
                         }
                     }
                 }

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/DestructorNetworkNode.java
@@ -100,6 +100,9 @@ public class DestructorNetworkNode extends NetworkNode implements IComparable, I
 
     private void pickupItems() {
         BlockPos front = pos.relative(getDirection());
+        if (!level.isLoaded(front)) {
+            return;
+        }
 
         List<ItemEntity> droppedItems = level.getEntitiesOfClass(ItemEntity.class, new AABB(front));
 
@@ -124,6 +127,9 @@ public class DestructorNetworkNode extends NetworkNode implements IComparable, I
 
     private void breakBlock() {
         BlockPos front = pos.relative(getDirection());
+        if (!level.isLoaded(front)) {
+            return;
+        }
         BlockState frontBlockState = level.getBlockState(front);
         Block frontBlock = frontBlockState.getBlock();
         ItemStack frontStack = frontBlock.getCloneItemStack(
@@ -174,6 +180,10 @@ public class DestructorNetworkNode extends NetworkNode implements IComparable, I
 
     private void breakFluid() {
         BlockPos front = pos.relative(getDirection());
+        if (!level.isLoaded(front)) {
+            return;
+        }
+
         BlockState frontBlockState = level.getBlockState(front);
         Block frontBlock = frontBlockState.getBlock();
 

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/FluidInterfaceNetworkNode.java
@@ -45,7 +45,7 @@ public class FluidInterfaceNetworkNode extends NetworkNode {
         protected void onContentsChanged() {
             super.onContentsChanged();
 
-            if (!level.isClientSide) {
+            if (!level.isClientSide && level.isLoaded(pos)) {
                 ((FluidInterfaceBlockEntity) level.getBlockEntity(pos)).getDataManager().sendParameterToWatchers(FluidInterfaceBlockEntity.TANK_IN);
             }
 

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/NetworkTransmitterNetworkNode.java
@@ -116,11 +116,11 @@ public class NetworkTransmitterNetworkNode extends NetworkNode {
             if (!isSameDimension()) {
                 Level dimensionLevel = level.getServer().getLevel(receiverDimension);
 
-                if (dimensionLevel != null && dimensionLevel.getBlockEntity(receiver) instanceof NetworkReceiverBlockEntity) {
+                if (dimensionLevel != null && dimensionLevel.isLoaded(receiver) && dimensionLevel.getBlockEntity(receiver) instanceof NetworkReceiverBlockEntity) {
                     operator.apply(dimensionLevel, receiver, null);
                 }
             } else {
-                if (level.getBlockEntity(receiver) instanceof NetworkReceiverBlockEntity) {
+                if (level.isLoaded(receiver) && level.getBlockEntity(receiver) instanceof NetworkReceiverBlockEntity) {
                     operator.apply(level, receiver, null);
                 }
             }

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/RootNetworkNode.java
@@ -107,7 +107,11 @@ public class RootNetworkNode implements INetworkNode, INetworkNodeVisitor {
     @Override
     public void visit(Operator operator) {
         for (Direction facing : Direction.values()) {
-            operator.apply(level, pos.relative(facing), facing.getOpposite());
+            BlockPos relativePos = pos.relative(facing);
+            if (!level.isLoaded(relativePos)) {
+                continue;
+            }
+            operator.apply(level, relativePos, facing.getOpposite());
         }
     }
 }

src/main/java/com/refinedmods/refinedstorage/blockentity/craftingmonitor/WirelessCraftingMonitor.java
@@ -4,15 +4,14 @@ import com.refinedmods.refinedstorage.RS;
 import com.refinedmods.refinedstorage.api.autocrafting.ICraftingManager;
 import com.refinedmods.refinedstorage.api.autocrafting.task.ICraftingTask;
 import com.refinedmods.refinedstorage.api.network.INetwork;
+import com.refinedmods.refinedstorage.blockentity.data.BlockEntitySynchronizationParameter;
 import com.refinedmods.refinedstorage.inventory.player.PlayerSlot;
 import com.refinedmods.refinedstorage.item.NetworkItem;
 import com.refinedmods.refinedstorage.item.WirelessCraftingMonitorItem;
 import com.refinedmods.refinedstorage.network.craftingmonitor.WirelessCraftingMonitorSettingsUpdateMessage;
-import com.refinedmods.refinedstorage.blockentity.data.BlockEntitySynchronizationParameter;
 import com.refinedmods.refinedstorage.util.NetworkUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
@@ -99,10 +98,13 @@ public class WirelessCraftingMonitor implements ICraftingMonitor {
 
     private INetwork getNetwork() {
         Level level = server.getLevel(nodeDimension);
-        if (level != null) {
-            return NetworkUtils.getNetworkFromNode(NetworkUtils.getNodeFromBlockEntity(level.getBlockEntity(nodePos)));
+        if (level == null) {
+            return null;
         }
-        return null;
+        if (!level.isLoaded(nodePos)) {
+            return null;
+        }
+        return NetworkUtils.getNetworkFromNode(NetworkUtils.getNodeFromBlockEntity(level.getBlockEntity(nodePos)));
     }
 
     public ItemStack getStack() {

commit 974774f6c31099deb7d83020af94dbf43baeda2c

Fix slight error introduced in 045b90a4

src/main/java/com/refinedmods/refinedstorage/apiimpl/network/node/NetworkNode.java
@@ -239,7 +239,7 @@ public abstract class NetworkNode implements INetworkNode, INetworkNodeVisitor {
         for (Direction facing : Direction.values()) {
             BlockPos facingPos = pos.relative(facing);
             if (!level.isLoaded(facingPos)) {
-                return;
+                continue;
             }
             INetworkNode oppositeNode = NetworkUtils.getNodeFromBlockEntity(level.getBlockEntity(facingPos));
             if (oppositeNode == null) {